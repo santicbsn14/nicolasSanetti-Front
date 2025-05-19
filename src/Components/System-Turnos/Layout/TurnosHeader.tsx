3b872acd64302a5f9d352bb9bb0cf9e36f936053
santiago_viale <santicbsn9@gmail.com>
2025-05-19 17:55:14 -0300

    construyendo diseño.4

--- src/Components/System-Turnos/Layout/TurnosHeader.tsx
+++ src/Components/System-Turnos/Layout/TurnosHeader.tsx
@@ -11,14 +11,24 @@ const TurnosHeader = () => {
           alt="Logo Nicolas Sanetti"
           className="turnos-header__logo"
         />
-        <div className="turnos-header__text">
-          <p className="turnos-header__line">🔥 ¿List@ para un cambio de look espectacular?</p>
-          <p className="turnos-header__line">
-            🕐 Horario: Lunes a Viernes de 9 a 20 hs. <br />
-            Sábados de 9 a 15 hs
-          </p>
-          <p className="turnos-header__line">📣 ¡Reservá tu turno y transformá tu cabello!</p>
-        </div>
+<div className="turnos-header__text">
+  <div className="turnos-header__line">
+    <span className="emoji">🔥</span>
+    <p>¿List@ para un cambio de look espectacular?</p>
+  </div>
+  <div className="turnos-header__line">
+    <span className="emoji">🕐</span>
+    <p>
+      Horario: Lunes a Viernes de 9 a 20 hs. <br />
+      Sábados de 9 a 15 hs
+    </p>
+  </div>
+  <div className="turnos-header__line">
+    <span className="emoji">📣</span>
+    <p>¡Reservá tu turno y transformá tu cabello!</p>
+  </div>
+</div>
+
       </div>
     </section>
   );
